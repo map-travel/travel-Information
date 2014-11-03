21d9557d9f018554d9361c69014bfe89627c3bb8
MORI <jsl1122@gmail.com>
2014-11-03 23:53:41 +0800

    login完成，先放暫代圖，等完成後更新login圖

--- TravelInformation/src/com/lccnet/android/travelinformation/LoginActivity.java
+++ TravelInformation/src/com/lccnet/android/travelinformation/LoginActivity.java
@@ -1,16 +1,32 @@
 package com.lccnet.android.travelinformation;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
 
 public class LoginActivity extends Activity {
- @Override
-protected void onCreate(Bundle savedInstanceState) {
-	// TODO Auto-generated method stub
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.activity_login);
-	
-	
-	
-}
+	private final int TIME_OUT=2000;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_login);
+
+		new Handler().postDelayed(new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				startActivity(new Intent(LoginActivity.this, WelcomeActivity.class));
+				LoginActivity.this.overridePendingTransition(R.anim.login_in, R.anim.login_out);
+				LoginActivity.this.finish();
+			}
+		}, TIME_OUT);
+
+
+	}
 }
