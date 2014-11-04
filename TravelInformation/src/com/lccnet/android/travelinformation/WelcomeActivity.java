a55f89351f658446b4997b09b925cc6ec71b0160
MORI <jsl1122@gmail.com>
2014-11-04 22:04:41 +0800

    建立側邊滑動選單

--- TravelInformation/src/com/lccnet/android/travelinformation/WelcomeActivity.java
+++ TravelInformation/src/com/lccnet/android/travelinformation/WelcomeActivity.java
@@ -1,34 +1,72 @@
 package com.lccnet.android.travelinformation;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
 
 public class WelcomeActivity extends Activity {
+
+	private DrawerLayout mDrawerLayout;
+	private ListView mDrawerList;
+	private ActionBarDrawerToggle mDrawerToggle;
+	private CharSequence mDarwTitle;
+	private CharSequence mTitle;
+	private Bundle saved;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_welcome);
+		saved = savedInstanceState;
+		InitView();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.welcome, menu);
-		return true;
+	private void InitView(){
+		mDrawerLayout=(DrawerLayout)findViewById(R.id.drawer_layout);
+		mDrawerList=(ListView)findViewById(R.id.left_menu);
+		mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow,
+				GravityCompat.START);
+		GetActionBar();
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+	private void GetActionBar(){
+		ActionBar actionbar=getActionBar();
+		actionbar.setDisplayHomeAsUpEnabled(true);
+		actionbar.setHomeButtonEnabled(true);
+
+		mDrawerToggle=new ActionBarDrawerToggle(this, mDrawerLayout, 
+				R.drawable.ic_drawer,	R.string.draw_open, R.string.draw_close){
+
+			@Override
+			public void onDrawerClosed(View view){
+				super.onDrawerClosed(view);
+				invalidateOptionsMenu();
+			}
+
+			@Override
+			public void onDrawerOpened(View drawerView){
+				super.onDrawerOpened(drawerView);
+				invalidateOptionsMenu();
+			}
+		};
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
 	}
+
+	 @Override
+	    public boolean onOptionsItemSelected(MenuItem item) {
+	        //home
+	        if (mDrawerToggle.onOptionsItemSelected(item)) { //按下App圖案可跳出選單
+	            return true;
+	        }
+			return super.onOptionsItemSelected(item);
+	 }
+
+
+
 }
